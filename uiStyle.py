d1ac3f650338681c8c9e67b3f94927cd42fed557
cclauss <cclauss@bluewin.ch>
2014-08-18 17:02:53 +0200

    Update uiStyle.py

--- uiStyle.py
+++ uiStyle.py
@@ -4,8 +4,8 @@ Style pythonista ui.View from one .pyui template.
 To use: 
     *create a ui view and add the controls that you would like styled.
     *Each control needs a unique name such as lbl1 - a style of label, lbl2 - another style of label.
-    *To access any of your styles in a projects ui.View controlls, just set the name of the style to the front of the
-        control name.
+    *To access any of your styles in a projects ui.View controlls, just set the name of the style to the front of
+        the control name.
     Example: Style ui.View has a lbl1. Your main view could have a lbl1name. the lbl1 is removed when the view is
         styled.
     
